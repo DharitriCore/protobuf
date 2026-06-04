780e89936930574bed6764a338cd8e94bd666c6a
Kasi Viswanath Aurangabad <kasi.aurangabadx@gmail.com>
2026-06-04 18:49:16 +0530

    Fix local proto regeneration paths

--- test/casttypewith/casttypewith.pb.go
+++ test/casttypewith/casttypewith.pb.go
@@ -657,10 +657,7 @@ func (m *Castaway) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthCasttypewith
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthCasttypewith
 			}
 			if (iNdEx + skippy) > l {
@@ -745,10 +742,7 @@ func (m *Wilson) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthCasttypewith
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthCasttypewith
 			}
 			if (iNdEx + skippy) > l {
